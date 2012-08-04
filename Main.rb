69b1b46b0642980d77cb247c0b22cd9f67c31761
Aaron Neyer <aaronneyer@gmail.com>
2012-08-04 16:26:08 -0400

    Added a bit to the README

--- Main.rb
+++ Main.rb
@@ -15,12 +15,15 @@ p2name = gets.chomp
 puts "What level is it?"
 p2level = gets.chomp.to_i
 
-p1 = eval("#{p1name.gsub(/\W/,'').downcase.capitalize}.new(#{p1level})")
-p2 = eval("#{p2name.gsub(/\W/,'').downcase.capitalize}.new(#{p2level})")
-
-battle = Battle.new(p1nick, p2nick, p1, p2)
-until battle.finished
-	puts "#{battle.turn.class} choose a move:"
-	puts battle.get_moves
-	puts battle.fight(gets.chomp.to_i)
+battle = Battle.new(p1nick, p2nick)
+battle.set_pokemon(p1nick, p1name, p1level)
+battle.set_pokemon(p2nick, p2name, p2level)
+if battle.pokemon1 && battle.pokemon2
+	until battle.finished
+		puts "#{battle.turn.class} choose a move:"
+		puts battle.get_moves
+		puts battle.fight(gets.chomp.to_i)
+	end
+else
+	puts "Someone tried to use a pokemon that does not exist(Only generation 1 are included)."
 end
